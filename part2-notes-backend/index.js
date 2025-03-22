260fdf1eed9c6eb60849ae4d19510ae7ba7b2ef8
Wondirad-Kifelew <wondiradabebekifelew@gmail.com>
2025-03-22 10:32:50 +0300

    initital commit

---
+++ part2-notes-backend/index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json()); //mid ware to parse the req.body
+app.use(cors());
+//mid ware to print request
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+app.use(requestLogger);
+// //mid ware to handle request to an unknown endpoint
+// const unknownEndpoint = (request, response) => {
+//   response.status(404).send({ error: "unknown endpoint" });
+// };
+
+// app.use(unknownEndpoint);
+
+let notes = [
+  { id: "1", content: "HTML is easy", important: true },
+  { id: "2", content: "Browser can execute only JavaScript", important: false },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+  {
+    id: "4",
+    content: "sdasdfasdfasdfasf",
+    important: true,
+  },
+];
+app.use(express.static('dist'))//serve static files
+//handle get request to the base url
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+//handle get request to the whole note res
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+//handle get request to a specific note
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+//handle delete request to a specific note
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id != id);
+  response.status(204).end();
+});
+//handle post request to add notes to the server
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return String(maxId + 1);
+};
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  console.log(note, "and the request header", request.headers);
+  response.json(note);
+});
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
